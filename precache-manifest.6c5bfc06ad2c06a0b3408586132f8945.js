1edda86bc400a52085d1cbe8307bdb5e7744cc89
prashant280920 <jayantmehta8447442229@gmail.com>
2020-05-31 19:30:36 +0530

    Updates

---
+++ precache-manifest.6c5bfc06ad2c06a0b3408586132f8945.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deca91a960558a739b28526a63119076",
+    "url": "/FaceRecognition_WebApp/index.html"
+  },
+  {
+    "revision": "579c4faf13875808f87c",
+    "url": "/FaceRecognition_WebApp/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "9a3fbb63e1ba9f814c2b",
+    "url": "/FaceRecognition_WebApp/static/css/main.c039f9ef.chunk.css"
+  },
+  {
+    "revision": "579c4faf13875808f87c",
+    "url": "/FaceRecognition_WebApp/static/js/2.74e467c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FaceRecognition_WebApp/static/js/2.74e467c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a3fbb63e1ba9f814c2b",
+    "url": "/FaceRecognition_WebApp/static/js/main.be07702e.chunk.js"
+  },
+  {
+    "revision": "df112e088382cbe7788f",
+    "url": "/FaceRecognition_WebApp/static/js/runtime-main.1f759f7c.js"
+  }
+]);
