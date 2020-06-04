f8378bc29190c0a8fa8e278272332ac75c3baab9
prashant280920 <jayantmehta8447442229@gmail.com>
2020-06-04 13:36:41 +0530

    added style

--- src/App.js
+++ src/App.js
@@ -2,17 +2,32 @@ import React from 'react';
 import Navigation from "./components/navigation/Navigation"
 import Logo from "./components/logo/Logo";
 import Rank from "./components/Rank/Rank";
+import Particles from 'react-particles-js';
 import ImageLinkForm from "./components/imageLinkForm/ImageLinkForm";
 import './App.css';
+
+const particlesOptions = {
+    "particles": {
+    	number: {
+    		value: 130,
+    		density:{
+    			enable: true,
+    			value_area: 800
+    		}
+    	}
+    },
+         
+}
 
 function App() {
   return (
     <div className="App">
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/*       <FaceRecognition>*/}
+    	<Particles className="particles" params={particlesOptions} />
+    	<Navigation />
+      	<Logo />
+	    <Rank />
+	    <ImageLinkForm />
+	    {/*       <FaceRecognition>*/}
     </div>
   );
 }
