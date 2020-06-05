ddf98f2b6784d8dbc10a64ae61e0c1128b9811de
prashant280920 <jayantmehta8447442229@gmail.com>
2020-06-05 16:11:50 +0530

    added array that contain data regarding face

--- src/components/faceRecognition/FaceRecognition.js
+++ src/components/faceRecognition/FaceRecognition.js
@@ -1,9 +1,23 @@
 import React from "react";
-
-const FaceRecognition = ({ imageUrl }) => {
+import './FaceRecognition.css';
+const creatediv = (box) => {
+		var a = <div className="bounding-box" style={{top:box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>;
+		return (a)
+		
+	}
+const FaceRecognition = ({ imageUrl, box }) => {
+	var i=0;
+	const item =[]
+	for(i=0;i<box.length;i++){
+		item.push(creatediv(box[i]))
+	}
 	return (
-		<div className="center">
-			<img src={imageUrl} alt='' />
+		<div className="center ma">
+			<div className="absolute mt2">
+			<img id="inputimage" src={imageUrl} alt='' width="500px" height="auto"/>
+			{item}
+			
+			</div>
 		</div>
 	);
 }
